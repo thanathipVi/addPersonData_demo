9e052de7dc22779865f25990999912607da506ca
ThanathipV <proaut.thanathip@gmail.com>
2025-11-27 19:52:33 +0700

    added model

--- addPersonDataDemo_api/Data/AppDbContext.cs
+++ addPersonDataDemo_api/Data/AppDbContext.cs
@@ -1,1 +1,12 @@
-// db context
+using Microsoft.EntityFrameworkCore;
+using PersonApi.Models;
+
+namespace PersonApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
