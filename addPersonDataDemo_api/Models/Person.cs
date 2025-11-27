a8e8ffef5ce90a650d2924c08149cf8794c73d52
ThanathipV <proaut.thanathip@gmail.com>
2025-11-27 19:53:04 +0700

    added model

--- addPersonDataDemo_api/Models/Person.cs
+++ addPersonDataDemo_api/Models/Person.cs
@@ -1,1 +1,17 @@
-// model
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonApi.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = "";
+
+        public DateTime BirthDate { get; set; }
+
+        public int Age { get; set; }
+    }
+}
