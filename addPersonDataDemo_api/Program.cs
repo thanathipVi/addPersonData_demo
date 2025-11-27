b8dc53e9ac90df034678de6b3f6e719a3b1542ec
ThanathipV <proaut.thanathip@gmail.com>
2025-11-27 19:53:38 +0700

    added c# class

--- addPersonDataDemo_api/Program.cs
+++ addPersonDataDemo_api/Program.cs
@@ -1,1 +1,34 @@
-// program
+using Microsoft.EntityFrameworkCore;
+using PersonApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("Default") ?? "Data Source=people.db"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.MapControllers();
+
+app.Run();
